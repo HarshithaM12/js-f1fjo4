e5f43bfcd3c6b477d19254724ed4b9e9fdfd5938
HarshithaM12 <115206433+HarshithaM12@users.noreply.github.com>
2022-10-11 23:46:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-f1fjo4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-f1fjo4)
---
+++ index.js
@@ -0,0 +1,33 @@
+/*function getCountryData(countryName){
+  return fetch(`https://restcountries.com/v2/name/${countryName}`);
+}
+
+function getNeighCountryData(countries){
+  const arr = [];
+  for(country of countries){
+    let p = getCountryData(country)
+    .then(res => res.json());
+    arr.push(p);
+  }
+  return Promise.all(arr);
+  }
+
+getCountryData('usa')
+.then(res => res.json())
+.then(data => {
+  console.log(data); 
+ return getNeighCountryData(data[0].borders);
+})
+.then(data => 
+  console.log(data));*/
+
+function getCountryData(countryName) {
+  return fetch(`https://restcountries.com/v2/name/${countryName}`);
+}
+
+getCountryData('usa')
+  .then((res) => res.json())
+  .then((countryData) => {
+    console.log(countryData[0]);
+  })
+  .catch((err) => console.log(err));
